d245e6f141f1eaf5a2681332c4a5151875da36c9
Tsugami <yslanramos.r@gmail.com>
2021-10-08 18:28:15 -0300

    first commit!

---
+++ src/math-parser.spec.ts
@@ -0,0 +1,51 @@
+import { parser, tokenizer } from "./math-parser"
+import type { Token } from './types';
+
+describe('MathParser', () => {
+    describe("tokenizer", () => {
+        it('should turn `input` string into `tokens` array', () => {
+            const input = "2 * ( 5 - 1 )"
+            const tokensExpect: Token[] = [
+                {
+                    type: 'number',
+                    value: '2'
+                },
+                {
+                    type: 'operator',
+                    value: '*'
+                },
+                {
+                    type: 'paren',
+                    value: '('
+                },
+                {
+                    type: 'number',
+                    value: '5'
+                },
+                {
+                    type: 'operator',
+                    value: '-'
+                },
+                {
+                    type: 'number',
+                    value: '1'
+                },
+                {
+                    type: 'paren',
+                    value: ')'
+                }
+            ]
+
+            expect(tokenizer(input)).toEqual(tokensExpect)
+        })
+    })
+
+
+    describe('parser', () => {
+        it('should return value sum', () => {
+            const input = "2 * 5 - 1"
+            
+            expect(parser(tokenizer(input))).toBe(9)
+        })
+    })
+})
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type TokenTypes = 'paren' | 'number' | 'operator'
+export type OperatorTypes = '*' | '^' | '-' | '+' | '/'
+
+type CommonToken = {
+    type: Exclude<TokenTypes, 'operator'>;
+    value: string;
+}
+
+
+export type OperatorToken = {
+    type: 'operator';
+    value: OperatorTypes;
+}
+
+
+export type Token = CommonToken | OperatorToken
